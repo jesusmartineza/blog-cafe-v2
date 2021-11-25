e63b3cba03a2ac4fc3792c8ffd4de0b951ea63b8
Jesus Martinez <jesusayala368@gmail.com>
2021-11-25 16:48:30 -0600

    Validacion formulario con js

---
+++ js/script.js
@@ -0,0 +1,67 @@
+//Eventos de la pagina contacto y validación del formulario
+
+//Variables
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+//EventListeners
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+//Evento del submit
+
+formulario.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    //Validar formulario
+    const {
+        nombre,
+        email,
+        mensaje
+    } = datos;
+
+    if (nombre === '' || email === '' || mensaje === '') {
+        mostrarAlerta('Todos los campos son obligatorios', true);
+
+        return; //Corta de ejecucion del codigo
+    }
+    //Envio de formulario correctamente
+    mostrarAlerta('El mensaje se ha enviado correctamente');
+
+
+});
+
+function leerTexto(e) {
+    datos[e.target.id] = e.target.value;
+
+    console.log(datos);
+}
+
+//Muestra alerta en HTML
+function mostrarAlerta(mensaje, error = null) {
+    const alerta = document.createElement('p')
+    alerta.textContent = (mensaje);
+
+    if (error) {
+        alerta.classList.add('error');
+    } else {
+        alerta.classList.add('envio');
+    }
+
+    formulario.appendChild(alerta);
+
+    //Desaparezca despues de 3 segundos
+    setTimeout(() => {
+        alerta.remove();
+    }, 4000);
+}
